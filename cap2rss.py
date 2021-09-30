95b0c10e55fc9ed7c48e39eb0cb1ad7ba7b62d56
ArieTheTrainGirl <arietraingirl@gmail.com>
2021-09-30 00:50:08 -0500

    Nap time :)

--- cap2rss.py
+++ cap2rss.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as ET
 import PyRSS2Gen
 import datetime
 ns = {'alert': 'urn:oasis:names:tc:emergency:cap:1.2'}
-capserverURL = "https://kj7bre.com/ipaws/server2server_bridge/ipaws.php?pin=209fbab696dcbe0adac2491335475a50&feed=public"
+capserverURL = "https://kj7bre.com/ipaws/server2server_bridge/ipaws.php?pin=209fbab696dcbe0adac2491335475a50&feed=eas"
 
 
 
@@ -27,26 +27,30 @@ description = "Integrated Public Alert Warning System Common Alert Protocol RSS 
 items = rssitemlist
 )
 
+while True:
 
-for alerts in xml.findall('./alert:alert', ns):
-            if alerts.find('alert:msgType', ns).text == "Alert":
-                try:
-                    CAPidentifier = alerts.find('alert:identifier', ns).text
-                    CAPsender = alerts.find('alert:sender', ns).text
-                    CAPinfo = alerts.find('alert:info', ns).text
-                    for info in alerts.findall('alert:info', ns):
-                        CAPdescription = info.find('alert:description', ns).text
-                        CAPevent = info.find('alert:event', ns).text
-                        print(CAPevent)
-                        rssitemlist.append(
-                            PyRSS2Gen.RSSItem(
-                            title = CAPevent,
-                            link = "https://www.wdfaradio.com/",
-                            description = CAPdescription,
-                            ),
-                        )
-                except:
-                    print("rip bozo")
-
-print(rssitemlist)
-rss.write_xml(open("eas.xml", "w"))
+    for alerts in xml.findall('./alert:alert', ns):
+                if alerts.find('alert:msgType', ns).text == "Alert":
+                    try:
+                        CAPidentifier = alerts.find('alert:identifier', ns).text
+                        CAPsender = alerts.find('alert:sender', ns).text
+                        CAPinfo = alerts.find('alert:info', ns).text
+                        for info in alerts.findall('alert:info', ns):
+                            CAPdescription = info.find('alert:description', ns).text
+                            CAPevent = info.find('alert:event', ns).text
+                            print(CAPevent)
+                            rssitemlist.append(
+                                PyRSS2Gen.RSSItem(
+                                title = CAPevent,
+                                link = "https://www.wdfaradio.com/",
+                                description = CAPdescription,
+                                ),
+                            )
+                    except:
+                        print("rip bozo")
+    try:
+        rss.write_xml(open("eas.xml", "w"))
+        print("FEMA IPAWS successfully converted to RSS!")
+    except:
+        print("FEMA IPAWS failed to write to XML...")
+    time.sleep(60)
